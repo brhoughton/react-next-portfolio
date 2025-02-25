672a444b5535b0e1e13e5d0b9651659534abcc0e
Breck Houghton <breck.houghton@gmail.com>
2025-02-25 14:13:06 -0800

    Updated copyright year and projects section

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 
 export default function Footer() {
-  return (
-    <footer className="px-4 mb-10 text-center text-gray-500">
-      <small className="block mb-2 text-xs ">
-        &copy; 2024 Breck Houghton. All rights reserved.
-      </small>
-      <p className="text-xs">
-        <span className="font-semibold">About this website:</span> built with
-        React and Next.js (App Router and Server Actions), TypeScript, Tailwind
-        CSS, Framer Motion, React Email and Resend, Vercel hosting.
-      </p>
-    </footer>
-  );
+	return (
+		<footer className="px-4 mb-10 text-center text-gray-500">
+			<small className="block mb-2 text-xs ">
+				&copy; 2025 Breck Houghton. All rights reserved.
+			</small>
+			<p className="text-xs">
+				<span className="font-semibold">About this website:</span> built with
+				React and Next.js (App Router and Server Actions), TypeScript, Tailwind
+				CSS, Framer Motion, React Email and Resend, Vercel hosting.
+			</p>
+		</footer>
+	);
 }
